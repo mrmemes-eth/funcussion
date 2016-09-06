07e4937952ac7144dcc25e50cbca297aacb4eff9
Sasha Heinen <sheinen@pivotal.io>
2016-09-06 16:12:08 -0600

    Make static library into dynamic framework for Carthage consumption
    Signed-off-by: Cody Sehl <csehl@pivotal.io>
    Signed-off-by: Sasha Heinen <sheinen@pivotal.io>
    
    wip2
    
    Signed-off-by: Sasha Heinen <sheinen@pivotal.io>
    
    wip3
    
    Signed-off-by: Cody Sehl <csehl@pivotal.io>
    
    wip4
    
    Signed-off-by: Sasha Heinen <sheinen@pivotal.io>
    
    wip5
    
    Signed-off-by: Cody Sehl <csehl@pivotal.io>
    Signed-off-by: Sasha Heinen <sheinen@pivotal.io>

---
+++ FuncussionDynamic/Funcussion.h
@@ -0,0 +1,22 @@
+//
+//  FuncussionDynamic.h
+//  FuncussionDynamic
+//
+//  Created by pivotal on 9/6/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Funcussion.
+FOUNDATION_EXPORT double FuncussionVersionNumber;
+
+//! Project version string for Funcussion.
+FOUNDATION_EXPORT const unsigned char FuncussionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Funcussion/PublicHeader.h>
+
+#import <Funcussion/NSArray+Funcussion.h>
+#import <Funcussion/NSDictionary+Funcussion.h>
+
+
